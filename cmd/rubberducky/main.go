a794afd2982e887a4a91ac9dac68845d0249487d
H <hackdom@majoolr.io>
2018-09-08 19:39:08 +0200

    start-cli

--- cmd/rubberducky/main.go
+++ cmd/rubberducky/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "rubberducky"
+	app.Usage = "finally share code on Ethereum!"
+	app.Action = func(c *cli.Context) error {
+		fmt.Printf("Hello %v", c.Args().Get(0))
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
